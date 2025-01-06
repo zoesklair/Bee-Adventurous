ad897faba4df1acf3cc7bb777bdc475967709ff1
zoesklair <zoe.sklair1@virginmedia.com>
2025-01-06 16:51:46 +0000

    energy display, placeholder sprite which flips

--- Assets/Scripts/Bee Scripts/Energy.cs
+++ Assets/Scripts/Bee Scripts/Energy.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Energy : MonoBehaviour
 {
     [SerializeField]
     TMP_Text energyDisplay;
+    [SerializeField]
+    GameObject energyBar;
+    Slider energyBarSlider;
+
     public float EnergyLevel => energyLevel;
 
     private float energyLevel;
     private float energyLevelReductionRate = 1;
-    
 
+    private void Awake()
+    {
+        energyBarSlider = energyBar.GetComponent<Slider>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -32,11 +40,15 @@ public class Energy : MonoBehaviour
     }
     void DisplayEnergy()
     {
+        //debug
         energyDisplay.text = "Energy Level: " + (int)EnergyLevel;
+
+        energyBarSlider.value = EnergyLevel / 100f;
+
     }
 
     public void UpdateEnergy(float energyChange)
     {
-        energyLevel += energyChange;
+        energyLevel = Mathf.Clamp(energyLevel + energyChange, 0, 100);
     }
 }
