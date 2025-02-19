5517b6c83b988bbf65e42a061cbcdbb25329af5a
zoesklair <zoe.sklair1@virginmedia.com>
2025-02-19 13:46:39 +0000

    score saving ui

--- Assets/Scripts/UI Scripts/GameOverScreen.cs
+++ Assets/Scripts/UI Scripts/GameOverScreen.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [SerializeField]
+    TMP_Text finalScoreDisplay;
+    [SerializeField]
+    TMP_InputField playerNameInputField;
+    ScoreController scoreController;
     private void Awake()
     {
         gameObject.SetActive(false);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        scoreController = GameObject.Find("ScoreController").GetComponent<ScoreController>();
     }
     public void MakeGameOverPanelActive()
     {
         gameObject.SetActive(true);
+        SetFinalScoreDisplay();
+    }
+    private void SetFinalScoreDisplay()
+    {
+        finalScoreDisplay.text = "Your score: " + scoreController.Score;
+    }
+    public void SavePlayerNameAndScore()
+    {
+        string playerName = playerNameInputField.text;
+        if (string.IsNullOrEmpty(playerName))
+        {
+            playerName = "Player";
+        }
+
+        Debug.Log("GameOverScreen.SavePlayerNameAndScore: save " + playerName + ", " + scoreController.Score);
     }
 }
